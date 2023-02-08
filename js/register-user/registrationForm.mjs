b640b8cc8ab92334dadb65588bfe53f61b11d52d
DrRuski <ilia_e@hotmail.com>
2023-02-08 09:39:59 +0100

    commit

--- js/register-user/registrationForm.mjs
+++ js/register-user/registrationForm.mjs
@@ -1,1 +1,14 @@
-export const userEmail = document.getElementById("userEmail")
+const userEmail = document.getElementById("userEmail");
+const userFullName = document.getElementById("fullName");
+const userName = document.getElementById("userName");
+const userPassword = document.getElementById("userPassword");
+const submitBtn = document.getElementById("submitBtn");
+
+export const userRegistrationInfo = {
+  method: "POST",
+  body: {
+    name: `${userName.value}`,
+    email: `${userEmail.value}`,
+    password: `${userPassword.value}`,
+  },
+};
