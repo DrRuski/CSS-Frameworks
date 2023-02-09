0e90813d428adeb5b4e4fe927432364257d97b8c
ddyrnes <dev.dyrnes@gmail.com>
2023-02-09 22:54:18 +0100

    Started createPost function

--- js/posts/fetchPosts.mjs
+++ js/posts/fetchPosts.mjs
@@ -1,3 +1,5 @@
+import { createPost } from "./writePosts.mjs";
+
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NzA3LCJuYW1lIjoiZGFkeSIsImVtYWlsIjoiRGFuRHlyNzcxNTdAc3R1ZC5ub3JvZmYubm8iLCJhdmF0YXIiOm51bGwsImJhbm5lciI6bnVsbCwiaWF0IjoxNjc1ODU1OTA4fQ.nAvo_mYNHr-_E9h9FndDDyw8mdvwL2yxOBpXVWH4R94";
 localStorage.setItem("accessToken", token);
@@ -15,9 +17,10 @@ export async function fetchPosts(url) {
       },
     };
     const response = await fetch(url, getData);
-    console.log(response);
-    const json = await response.json();
-    console.log(json);
+    // console.log(response);
+    const postData = await response.json();
+    console.log(postData);
+    createPost();
   } catch (error) {
     console.log(error);
   }
