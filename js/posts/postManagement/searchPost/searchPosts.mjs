72f4bccb758e2b3137d8030aa5f147fd3e32a896
DrRuski <ilia_e@hotmail.com>
2023-02-20 10:27:35 +0100

    New search event, fully done, ready for review.

--- js/posts/postManagement/searchPost/searchPosts.mjs
+++ js/posts/postManagement/searchPost/searchPosts.mjs
@@ -2,20 +2,56 @@ import { renderPosts } from "../../renderPosts.mjs";
 import { getSearchTerms } from "./getTerms.mjs";
 import { postNotFoundError } from "./postNotFound.mjs";
 
+// export async function searchPosts(postData, url) {
+//   const postsArray = await postData(url);
+//   const searchForm = document.querySelector("form#searchForm");
+//   searchForm.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     const form = e.target;
+//     const searchTerm = form.search.value;
+//     const term = searchTerm.toLowerCase();
+//     const postContainer = document.querySelector(".write-post");
+//     postContainer.innerHTML = "";
+
+//     if (!getSearchTerms(postsArray, term).length) {
+//       return renderPosts(postsArray);
+//     } else {
+//       if (getSearchTerms(postsArray, term).length) {
+//         return renderPosts(getSearchTerms(postsArray, term));
+//       } else {
+//         return postNotFoundError(postContainer);
+//       }
+//     }
+//   });
+// }
+
 export async function searchPosts(postData, url) {
   const postsArray = await postData(url);
-  const searchForm = document.querySelector("form#searchForm");
-  searchForm.addEventListener("submit", (e) => {
+  const searchForm = document.querySelector("input#searchInput");
+  const postContainer = document.querySelector(".write-post");
+  //
+  searchForm.addEventListener("input", onSearch);
+  //
+  function onSearch(e) {
     e.preventDefault();
-    const form = e.target;
-    const searchTerm = form.search.value;
+    const searchTerm = e.target.value;
     const term = searchTerm.toLowerCase();
-    const postContainer = document.querySelector(".write-post");
-    postContainer.innerHTML = "";
-    if (getSearchTerms(postsArray, term).length) {
-      renderPosts(getSearchTerms(postsArray, term));
-    } else {
-      postNotFoundError(postContainer);
+    if (!term.length) {
+      postContainer.innerHTML = "";
+      return renderPosts(postsArray);
     }
-  });
+    //
+    if (term.length < 3) {
+      return;
+    }
+    //
+    postContainer.innerHTML = "";
+    //
+    if (getSearchTerms(postsArray, term).length) {
+      return renderPosts(getSearchTerms(postsArray, term));
+    } else {
+      return postNotFoundError(postContainer);
+    }
+  }
+  //
 }
