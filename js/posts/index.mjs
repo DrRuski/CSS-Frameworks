7e9846b70a0aa1123076bea3de1c01fc733c3c57
DrRuski <ilia_e@hotmail.com>
2023-03-03 15:31:29 +0100

    shortening some code

--- js/posts/index.mjs
+++ js/posts/index.mjs
@@ -1,4 +1,8 @@
-import { api_Base_Url, api_All_Posts_EndPoint, api_Create_New_Post_EndPoint } from "../api/api_Url_Endpoints.mjs";
+import {
+  api_Base_Url,
+  api_All_Posts_EndPoint,
+  api_Create_New_Post_EndPoint,
+} from "../api/api_Url_Endpoints.mjs";
 import { getPosts } from "./getPosts.mjs";
 import { setupPage } from "./setupPage.mjs";
 import { searchPosts } from "./postManagement/searchPost/searchPosts.mjs";
@@ -8,10 +12,18 @@ setupPage(`${api_Base_Url}${api_All_Posts_EndPoint}?_author=true`);
 //
 searchPosts(getPosts, `${api_Base_Url}${api_All_Posts_EndPoint}?_author=true`);
 //
-const newPostForm = document.querySelector("form#newPostForm");
-newPostForm.addEventListener("submit", async (e) => {
-  e.preventDefault();
-  await publishPost(`${api_Base_Url}${api_Create_New_Post_EndPoint}`);
-  location.reload();
-});
+document
+  .querySelector("form#newPostForm")
+  .addEventListener("submit", async (e) => {
+    e.preventDefault();
+    await publishPost(`${api_Base_Url}${api_Create_New_Post_EndPoint}`);
+    location.reload();
+  });
 //
+
+// const newPostForm = document.querySelector("form#newPostForm");
+// newPostForm.addEventListener("submit", async (e) => {
+//   e.preventDefault();
+//   await publishPost(`${api_Base_Url}${api_Create_New_Post_EndPoint}`);
+//   location.reload();
+// });
