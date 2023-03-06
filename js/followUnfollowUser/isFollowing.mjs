c3f29f9b602d4a169bbae420c97c0e760cca50ff
ddyrnes <dev.dyrnes@gmail.com>
2023-03-06 19:09:06 +0100

    More updates to isFollowing.mjs, filter posts

--- js/followUnfollowUser/isFollowing.mjs
+++ js/followUnfollowUser/isFollowing.mjs
@@ -4,34 +4,55 @@ import { checkFollow } from "./index.mjs";
 export function isFollowing(follower) {
   function getData() {
     const followers = viewFollowers(follower);
-    console.log(followers);
     const users = buttons();
-    if (followers === users) {
-      console.log("test");
-    }
+    // console.log(followers);
+    // console.log(users);
+    const test = ["IlyaaTest", "antonio", "torstein123", "12312321123", "dady", "Fiona_Macka"];
+    // console.log(test);
+    // let result = test.filter((e1) => !users.some((e2) => e1 === e2));
+    // console.log("Filter Results: " + result);
+    // const allFollowers = test.filter((item) => !users.includes(item));
+    // console.log(allFollowers);
+    console.log(followers);
+    let arr1 = followers;
+    let arr2 = users;
+    arr1 = arr1.filter(function (item) {
+      return arr2.includes(item);
+    });
+    console.log(arr1);
   }
   getData();
 }
 
 function viewFollowers(follower) {
-  const viewFollowersVariable = follower.followers.forEach((element) => {
-    const followers = element.name;
-    // console.log(followers);
-    return followers;
-  });
-  console.log(viewFollowersVariable);
+  const viewFollowersVariable = follower.followers.map((element) => element.name);
   return viewFollowersVariable;
 }
 
 function buttons() {
   const btn = document.querySelectorAll(".follow-user");
-  const btnVariable = btn.forEach((e) => {
-    const users = e.id;
-    // console.log(users);
-    return users;
-  });
+  const btnVariable = Array.from(btn).map((element) => element.id);
   return btnVariable;
 }
+
+// function viewFollowers(follower) {
+//   const viewFollowersVariable = follower.followers.forEach((element) => {
+//     const followers = element.name;
+//     // console.log(followers);
+//     return followers;
+//   });
+//   console.log(viewFollowersVariable);
+//   return viewFollowersVariable;
+// }
+// function buttons() {
+//   const btn = document.querySelectorAll(".follow-user");
+//   const btnVariable = btn.forEach((e) => {
+//     const users = e.id;
+//     // console.log(users);
+//     return users;
+//   });
+//   return btnVariable;
+// }
 
 // function checkIfFollowing() {
 //   if (followers === users) {
