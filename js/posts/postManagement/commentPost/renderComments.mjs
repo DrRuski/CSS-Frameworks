b2c3c3946203c091c8e5d24c864447132be1c11b
DrRuski <ilia_e@hotmail.com>
2023-03-10 14:05:24 +0100

    What about now?

--- js/posts/postManagement/commentPost/renderComments.mjs
+++ js/posts/postManagement/commentPost/renderComments.mjs
@@ -1,11 +1,23 @@
 export function renderComments(postData) {
   const test = document.querySelector(".commentTester");
+  const commentContainer = document.createElement("li");
+  commentContainer.setAttribute(
+    "id",
+    postData.map((e) => e.id)
+  );
+  commentContainer.classList.add(
+    "list-group-item",
+    "d-flex",
+    "flex-column",
+    "gap-2"
+  );
+  test.appendChild(commentContainer);
   postData.map((e) => {
     const commentUsername = e.author.name;
     const commentUserAvatar = e.author.avatar;
     const commentBody = e.body;
 
-    test.innerHTML += `  <li class="list-group-item d-flex flex-column gap-2">
+    commentContainer.innerHTML += `  
                                 <div class="d-flex align-items-center gap-3">
                                     <img src="${commentUserAvatar}" class="img-fluid card-author-img" alt="">
                                     <h5 class="m-0">${commentUsername}</h5>
@@ -13,7 +25,6 @@ export function renderComments(postData) {
                                 <div>
                                     <p class="m-0">${commentBody}</p>
                                 </div>
-                            </li>
                             <div class="border-top my-2 p-1"></div>`;
   });
 }
