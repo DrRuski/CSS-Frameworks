a8d913487fc87eec2ddd8c7675915964079f0011
DrRuski <ilia_e@hotmail.com>
2023-03-11 12:44:30 +0100

    It all Works... i think

--- js/posts/postManagement/commentPost/renderComments.mjs
+++ js/posts/postManagement/commentPost/renderComments.mjs
@@ -1,28 +1,25 @@
 export function renderComments(postData) {
-  console.log(postData);
-  const commentContainer = document.querySelector(".commentContainer");
-  
-  
+  postData.forEach((e) => {
+    const commentContainer = document.querySelector(".commentContainer");
+    const commentItem = document.createElement("li");
+    commentItem.classList.add(
+      "list-group-item",
+      "d-flex",
+      "flex-column",
+      "gap-2"
+    );
+    commentContainer.appendChild(commentItem);
+    commentItem.innerHTML = `<div class="d-flex align-items-center gap-3">
+      <img src="" class="img-fluid card-author-img userAvatar" alt="">
+      <h5 class="m-0 userName">d</h5>
+    </div>
+    <div>
+      <p class="m-0 commentBody"></p>
+    </div>
+    <div class="border-top my-2 p-1"></div>`;
 
-  commentContainer.innerHTML = `
-                                <li class="list-group-item d-flex flex-column gap-2 commentItem">
-                                <div class="d-flex align-items-center gap-3">
-                                    <img src="" class="img-fluid card-author-img userAvatar" alt="">
-                                    <h5 class="m-0 userName"></h5>
-                                </div>
-                                <div>
-                                    <p class="m-0 commentBody"></p>
-                                </div>
-                              <div class="border-top my-2 p-1"></div>
-                              </li>`;
-  
-  
-postData.map((e) => {
-  const commentItem = document.querySelector(".commentItem");
-  console.log(commentItem);
-  commentItem.querySelector(".userAvatar").src = e.author.avatar;
-  commentItem.querySelector(".userName").innerText = e.author.name;
-  commentItem.querySelector(".commentBody").innerText = e.body;
-});
- 
+    commentItem.querySelector(".userAvatar").src = e.author.avatar;
+    commentItem.querySelector(".userName").innerText = e.author.name;
+    commentItem.querySelector(".commentBody").innerText = e.body;
+  });
 }
